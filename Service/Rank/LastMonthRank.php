c24f1f4d205c6a705c01a94a77b79fb94ebcf831
kurozumi <info@a-zumi.net>
2021-12-22 12:33:15 +0900

    first commit

---
+++ Service/Rank/LastMonthRank.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace Customize\Service\Rank;
+
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\ORM\EntityManagerInterface;
+use Eccube\Entity\Customer;
+use Eccube\Entity\Master\OrderItemType;
+use Eccube\Entity\Master\OrderStatus;
+use Eccube\Entity\Order;
+use Eccube\Entity\OrderItem;
+use Eccube\Entity\Shipping;
+use Plugin\CustomerGroup\Entity\Group;
+use Plugin\CustomerGroupRank\Service\Rank\RankInterface;
+
+class LastMonthRank implements RankInterface
+{
+    /**
+     * @var EntityManagerInterface
+     */
+    protected $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+
+    /**
+     * 優先度が最上位のグループを会員に設定する
+     *
+     * @param Customer $customer
+     * @return void
+     */
+    public function decide(Customer $customer): void
+    {
+        // 会員グループをクリアする
+        $customer->getGroups()->clear();
+
+        // 対象の会員ブループが見つかったら登録
+        $groups = $this->getGroups($customer);
+        if ($groups->count() > 0) {
+            /** @var Group $group */
+            $group = $groups->first();
+            $customer->addGroup($group);
+        }
+    }
+
+    protected function getGroups(Customer $customer): ArrayCollection
+    {
+        $last_month_first = (new \DateTimeImmutable('first day of previous month'))->setTime(0, 0, 0);
+        $last_month_last = (new \DateTimeImmutable('last day of previous month'))->setTime(23, 59, 59);
+
+        $orderStatus = $this->entityManager->find(OrderStatus::class, OrderStatus::DELIVERED);
+        $orderItemType = $this->entityManager->find(OrderItemType::class, OrderItemType::PRODUCT);
+
+        $qb = $this->entityManager->createQueryBuilder();
+        $qb
+            ->select('SUM(oi.price * oi.quantity)')
+            ->from(OrderItem::class, 'oi')
+            ->innerJoin(Order::class, 'o', 'WITH', 'oi.Order = o.id')
+            ->innerJoin(Shipping::class, 's', 'WITH', 'o.id = s.Order')
+            ->where('oi.OrderItemType = :OrderItemType')
+            ->andWhere('o.Customer = :Customer')
+            ->andWhere('o.OrderStatus = :OrderStatus')
+            ->andWhere('s.shipping_date BETWEEN :last_month_first AND :last_month_last')
+            ->setParameter('Customer', $customer)
+            ->setParameter('OrderStatus', $orderStatus)
+            ->setParameter('OrderItemType', $orderItemType)
+            ->setParameter('last_month_first', $last_month_first)
+            ->setParameter('last_month_last', $last_month_last);
+
+        $buyTotal = $qb->getQuery()->getSingleScalarResult();
+
+        // 注文回数は条件に入れないので0指定
+        $searchData = [
+            'buyTimes' => 0,
+            'buyTotal' => $buyTotal ?? 0
+        ];
+
+        $groups = $this->entityManager->getRepository(Group::class)->getQueryBuilderBySearchData($searchData)
+            ->getQuery()
+            ->getResult();
+
+        return new ArrayCollection($groups);
+    }
+}
---
+++ Tests/Service/Rank/LastMonthRankTest.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace Customize\Tests\Service\Rank;
+
+use Eccube\Entity\Customer;
+use Eccube\Entity\Master\OrderStatus;
+use Eccube\Tests\EccubeTestCase;
+use Plugin\CustomerGroup\Tests\TestCaseTrait;
+use Plugin\CustomerGroupRank\Service\Rank\Context;
+
+class LastMonthRankTest extends EccubeTestCase
+{
+    use TestCaseTrait;
+
+    /**
+     * @var Context
+     */
+    protected $context;
+
+    public function setUp()
+    {
+        parent::setUp();
+
+        $this->context = self::$container->get(Context::class);
+    }
+
+    /**
+     * @param $buy_total
+     * @param $price
+     * @param $quantity
+     * @param $expected
+     *
+     * @dataProvider decideProvider
+     */
+    public function testDecide($buy_total, $price, $quantity, $datetime, $hour, $minute, $second, $expected)
+    {
+        $group = $this->createGroup();
+        $group
+            ->setBuyTimes(1)
+            ->setBuyTotal($buy_total);
+
+        $customer = $this->createCustomer();
+
+        $last_month_first = (new \DateTimeImmutable($datetime))->setTime($hour, $minute, $second);
+
+        $orderStatus = $this->entityManager->find(OrderStatus::class, OrderStatus::DELIVERED);
+
+        $order = $this->createOrder($customer);
+        foreach ($order->getOrderItems() as $orderItem) {
+            if ($orderItem->isProduct()) {
+                $shipping = $orderItem->getShipping();
+                $shipping->setShippingDate($last_month_first);
+                $orderItem
+                    ->setShipping($shipping)
+                    ->setPrice($price)
+                    ->setQuantity($quantity);
+
+                $order->setOrderStatus($orderStatus);
+                $this->entityManager->persist($order);
+            }
+        }
+
+        $this->entityManager->flush();
+
+        $this->context->decide($customer);
+
+        $groups = $this->entityManager->find(Customer::class, $customer->getId())->getGroups();
+
+        self::assertCount($expected, $groups);
+    }
+
+    public function decideProvider()
+    {
+        return [
+            [1000, 1000, 1, 'first day of previous month', 0, 0, 0, 1],
+            [1000, 100, 1, 'first day of previous month', 0, 0, 0, 0],
+            [1000, 1000, 1, 'last day of previous month', 23, 59, 59, 1],
+            [1000, 100, 1, 'last day of previous month', 23, 59, 59, 0],
+            [1000, 1000, 1, 'previous month', 0, 0, 0, 1],
+        ];
+    }
+}
